bf3d3dce3d57d23d82dd3c65c45d2a05512421b3
kondo <kondo@qumu.co.jp>
2023-11-20 20:20:39 +0900

    add sample d3 code

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,17 +1,46 @@
 <template>
-    <div>
-        {{ title }}
-    </div>
+  <svg class="graph"></svg>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted } from 'vue';
+import d3 from '../d3';
 
+const width = 640;
+const height = 400;
+const marginTop = 20;
+const marginRight = 20;
+const marginBottom = 30;
+const marginLeft = 40;
 
-const title = ref('title deploy test 1')
+onMounted(() => {
+
+  const x = d3.scale.scaleUtc()
+    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+    .range([marginLeft, width - marginRight]);
+
+  // Declare the y (vertical position) scale.
+  const y = d3.scale.scaleLinear()
+    .domain([0, 100])
+    .range([height - marginBottom, marginTop]);
+
+  // Create the SVG container.
+  const svg = d3.selection.select(".graph")
+    .attr("width", width)
+    .attr("height", height);
+
+  // Add the x-axis.
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axis.axisBottom(x));
+
+  // Add the y-axis.
+  svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axis.axisLeft(y));
+
+})
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/d3.ts
@@ -0,0 +1,11 @@
+import * as d3scale from 'd3-scale';
+import * as d3selection from 'd3-selection';
+import * as d3axis from 'd3-axis';
+
+const d3 = {
+    scale: d3scale,
+    selection: d3selection,
+    axis: d3axis,
+};
+
+export default d3;
