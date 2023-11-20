cda29c65e5df0a9469f6c4768c94a6f7a2a96903
kondo <kondo@qumu.co.jp>
2023-11-20 13:46:24 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <Graph />
+</template>
+
+<script setup lang="ts">
+import Graph from './components/Graph.vue';
+
+</script>
---
+++ src/components/Graph.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const title = ref('title')
+
+</script>
+
+<style scoped>
+
+</style>
